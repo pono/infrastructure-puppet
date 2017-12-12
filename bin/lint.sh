e58d3241307b6fab7196aff7ed935a9d1e3d4581
Daniel Pono Takamori <pono@apache.org>
2017-12-12 13:50:08 -1000

    ch-ch-ch-ch-chaaaaanges 7

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-bundle exec puppet-lint --no-nested_classes_or_defines-check --no-autoloader_layout-check $(git --no-pager show --pretty="" --name-only| tr '\r\n' ' ')
+export commit=$(git rev-parse HEAD)
+export files=$(git --no-pager show --pretty="" --name-only| tr '\r\n' ' ')
+bundle exec puppet-lint --no-nested_classes_or_defines-check --no-autoloader_layout-check -r $commit $files
